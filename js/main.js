b6d7355237155f51187fbdf4f405d8975a75309d
ElisaME <elisam.esquivel@gmail.com>
2016-05-24 13:30:10 -0500

    Arregle contador de intentos, solo falta sacar img de array para finalizar juego

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
-$(document).ready(function(){
+
+   var num;
+   
    var imagenes = [
       "Alma Patricia Jimenez.JPG",  "Ana Maria Barbosa.JPG",
       "Analy Miranda.JPG",          "Areli Rodriguez.JPG",
@@ -7,7 +9,7 @@ $(document).ready(function(){
       "Evelyn Vázquez.JPG",         "Gabriela Peñaloza.JPG",
       "Grissel Coutiño.JPG",        "Guadalupe Guerrero.JPG",
       "Johana Alexa Vargas.JPG",    "Joyce Zeltzin Hernández.JPG",
-      "Juana Ofelia García.JPG",    "Karen A. Sha ron De Diego.JPG",
+      "Juana Ofelia García.JPG",    "Karen A. Sharon De Diego.JPG",
       "Karen Cruz Heredia.JPG",     "Karen Quiroz.JPG",
       "Karla Monica Llerenas.JPG",  "Karla Vargas.JPG",
       "Leslie Anigail Vazquez.JPG", "Lilian Mishel Martínez.JPG",
@@ -22,7 +24,7 @@ $(document).ready(function(){
       "Daniela", "Elisa", "Evelyn", "Gabriela", "Griss", "Lupita", "Johana", 
       "Joyce", "Ofelia", "Sharon", "Karen", "Karen", "Monica", "Karla", "Leslie", 
       "Lilian", "Milca", "Naibit", "Nayeli", "Nelly", "Reyna", "Adriana", "Ruth",
-      "Ruth", "Sandra", "San", "Vianey", "Zazil"
+      "Ruth", "Sandra", "San", "Vian", "Zaz"
    ];
 
    //Funcion numero Aleatorio, para elegir imagen de array
@@ -32,12 +34,17 @@ $(document).ready(function(){
       return entero;
    };
   
-   
    //Inserta imagen aleatoria en el html FUNCIONA!
    function imagenAleatoria(){
       var tamano= imagenes.length;
       var num=numeroAleatorio(0,tamano);
-      
+      //Se coloca en el input, selecciona el contenido para no tener q borrarlo
+      $('#inputNombre').focus();
+      $('#inputNombre').select();
+      //asigno valor a intentos e i para contador de intentos
+      intentos=1
+      i=4
+      //Cambia atributos de src y alt a imagenes
       var rutaImagen='fotos/' + imagenes[num];
       var altImagen=nombres[num]
       $('#imagenAlumna').attr({
@@ -45,7 +52,7 @@ $(document).ready(function(){
          'alt':altImagen
       });
    };
-   
+
    //Cuando seleccionen la opcion Mexico se llama a la funcion imagenAleatoria
    $('#sedes').change(function(){
       var sede=$(this).val();
@@ -61,25 +68,28 @@ $(document).ready(function(){
    var intentos=1
    var i=4
 
+$(document).ready(function(){
    //Comprobar respuesta
    $('#btnConfirmar').click(function(){
       //FUNCIONA la comparacion
       var nombre=$('#inputNombre').val().toLowerCase();
       var nombreImagen=$("#imagenAlumna").attr("alt").toLowerCase();
       //En cada click se incrementa el numero de intentos
-      intentos++
+      intentos++      
       //Comparar nombre de imagen con input
       if(nombre==nombreImagen){
          alert("Bien hecho!");
          imagenAleatoria();
          totalPuntos+=5
          $("span").text(totalPuntos);
+         // nombres.splice(num,1)
+         // imagenes.splice(num,1)
       }else{
          if(intentos<=5){
-            alert("Incorrecto tienes " + i + "intentos mas");
+            alert("Incorrecto tienes " + i + " intentos mas");
             i--
             // intentos++
-         } else {
+         } else {//Cuando los intentos han llegado a 5
             alert("Lo siento se acabaron tus intentos! Prueba con otra imagen");
             imagenAleatoria();
             totalPuntos--
@@ -89,5 +99,4 @@ $(document).ready(function(){
    });
 });
    
-
-   
+ 
